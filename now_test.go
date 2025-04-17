ac17fc95c6d1e4784d8d857dab04dba92a870f87
zen <hasee>
2025-04-17 09:17:45 +0800

    test(now_test): 添加测试报告生成功能
    在now_test.go中添加代码以生成report.md文件，用于记录天气数据的测试结果。该文件包含表格格式的天气数据，便于查看和分析。

--- now_test.go
+++ now_test.go
@@ -1,10 +1,13 @@
 package qweather
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
 	"github.com/zhangyiming748/pretty"
 	"github.com/zhangyiming748/qweather/constant"
-	"log"
-	"testing"
 )
 
 func init() {
@@ -16,6 +19,15 @@ func TestNow(t *testing.T) {
 		t.Error(err)
 	} else {
 		pretty.P(rep)
+		if report,err :=os.OpenFile("report.md", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644); err!= nil {
+			t.Error(err)	
+		}else{
+			defer report.Close()
+			pretty.P(rep, report)
+			report.WriteString(fmt.Sprintln("|数据观测时间|温度|体感温度|天气状况图标代码|天气状况描述|风向360角度|风向|风力等级|风速|相对湿度|降水量|大气压强|能见度|云量|露点温度|"))
+			report.WriteString(fmt.Sprintln("|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"))
+			report.WriteString(fmt.Sprintf("|%v|%v|%v|%v|%v|%v|%v|%v|%v|%v|%v|%v|%v|%v|%v|", rep.Now.ObsTime, rep.Now.Temp, rep.Now.FeelsLike, rep.Now.Icon, rep.Now.Text, rep.Now.Wind360, rep.Now.WindDir, rep.Now.WindScale, rep.Now.WindSpeed, rep.Now.Humidity, rep.Now.Precip, rep.Now.Pressure, rep.Now.Vis, rep.Now.Cloud, rep.Now.Dew))
+		}
 	}
 
 }
